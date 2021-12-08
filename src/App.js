7c6b7a40590c0d2cef0a0c062179b73f947b1a73
AlyssaG565 <90293939+AlyssaG565@users.noreply.github.com>
2021-12-07 23:33:28 -0500

    runs qr code and scanner yay

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+}from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './pages/Home'
+import QRgen from './pages/QRgenerator'
+import QRscan from './pages/QRscanner'
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }))
+        .catch(err => err);
 }
 
+componentWillMount() {
+  this.callAPI();
+}
+
+render(){
+  return(
+    <div className = "App">
+      <div className="App-header">
+
+      
+      <img src ={logo} className= "App-logo" alt="logo"/>
+        <h1 className="App-title">Welcome to React!</h1>
+      <p className="App-intro">{this.state.apiResponse}</p>
+        <Router>
+          <div>
+            <Switch>
+            <Route exact path="/">
+                <Home/>
+              </Route>
+              <Route path="/qr_generator">
+                <QRgen/>
+              </Route>
+              <Route path="/qr_scanner">
+              <QRscan/>
+              </Route>
+            </Switch> 
+
+      </div>
+      </Router>
+     </div>
+    </div>
+    
+    );
+  }
+}
 export default App;
